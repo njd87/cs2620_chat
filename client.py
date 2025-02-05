49f66f3de82e96fa3df8bf6b2bc0d2c7a932d856
Nicholas DeSanctis <njdesanctis@gmail.com>
2025-02-05 00:00:25 -0500

    added documentation for server.py; need to wkr on client.py

--- client.py
+++ client.py
@@ -12,7 +12,26 @@ port = int(sys.argv[2])
 
 sel = selectors.DefaultSelector()
 
-def start_connection(host, port, selector):
+
+def start_connection(
+                    host: str,
+                    port: int,
+                    selector: selectors.DefaultSelector
+                    ) -> tuple[socket.socket, types.SimpleNamespace]:
+    '''
+    Start a connection to the server.
+
+    Parameters
+    ----------
+    host : str
+        The host to connect to.
+    port : int
+        The port to connect to.
+    selector : selectors.DefaultSelector
+        Where to register the connection.
+    '''
+
+    # Create a socket and connect to the server.
     server_addr = (host, port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(False)
@@ -21,12 +40,24 @@ def start_connection(host, port, selector):
     except Exception as e:
         print("Connection error:", e)
         sys.exit(1)
+    
+    # Register the socket with the selector to send events.
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     data = types.SimpleNamespace(addr=server_addr, inb=b"", outb=b"")
     selector.register(sock, events, data=data)
     return sock, data
 
 def service_connection(key, mask):
+    '''
+    Service a connection.
+
+    Parameters
+    key : selectors.SelectorKey
+        The key for the connection.
+    mask : int
+        The mask of events that occurred on the connection.
+    '''
+    # ge the socket
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
--- server.py
+++ server.py
@@ -14,52 +14,110 @@ if not os.path.exists(log_file):
         pass
 logging.basicConfig(filename=log_file, level=logging.INFO)
 
-sel = selectors.DefaultSelector()
+def setup():
+    '''
+    Set up the server.
+    '''
+    global sel, log_file
 
-host, port = sys.argv[1], int(sys.argv[2])
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-logging.info("Listening on %s:%d at %s", host, port, time.strftime("%Y-%m-%d %H:%M:%S"))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
+    # check if the log file exists
+    if not os.path.exists(log_file):
+        logging.error("Log file does not exist, exiting at %s", time.strftime("%Y-%m-%d %H:%M:%S"))
+        sys.exit(1)
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
+    # create basic selector
+    sel = selectors.DefaultSelector()
+
+    # check arguments for host and port
+    host, port = sys.argv[1], int(sys.argv[2])
+
+    # set up socket
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind((host, port))
+    lsock.listen()
+    logging.info("Listening on %s:%d at %s", host, port, time.strftime("%Y-%m-%d %H:%M:%S"))
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
+def accept_wrapper(sock: socket.socket) -> None:
+    '''
+    Accept a connection and register it with the selector.
+
+    Parameters
+    ----------
+    sock : socket.socket
+        The socket to accept a connection on.
+    '''
+    conn, addr = sock.accept()
     logging.info("Accepted connection from %s at %s", addr, time.strftime("%Y-%m-%d %H:%M:%S"))
     conn.setblocking(False)
+
+    # register the connection with the selector
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
 
-def service_connection(key, mask):
+def service_connection(key, mask: int) -> None:
+    '''
+    Takes a key and mask from the selector and handles the connection.
+
+    Parameters
+    ----------
+    key : selectors.SelectorKey
+       Contains the file object, events, and data for the connection.
+
+    mask : int
+        The mask of events that occurred on the connection.
+        1 = read
+        2 = write
+        3 = read and write
+    '''
+
+    # get the socket and data from the key
     sock = key.fileobj
     data = key.data
+
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(4096)  # Should be ready to read
+        # read the data that was sent and parse it
+        # for now, it just echos back the data
+        recv_data = sock.recv(4096)
         if recv_data:
             data.outb += recv_data
         else:
-            logging.info('Closing connection to %s at %s', data.addr, time.strftime("%Y-%m-%d %H:%M:%S"))
+            # some error occurred, close the connection
+            logging.error('Closing connection to %s at %s', data.addr, time.strftime("%Y-%m-%d %H:%M:%S"))
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
+        # if there is data to send, send it
         if data.outb:
             logging.info("Echoing %r to %s at %s", data.outb, data.addr, time.strftime("%Y-%m-%d %H:%M:%S"))
-            sent = sock.send(b'Message Received')  # Should be ready to write
+            sent = sock.send(data.outb)
             data.outb = data.outb[sent:]
 
+def main_loop() -> None:
+    '''
+    Main loop for the server.
+    '''
+    try:
+        while True:
+            # listen for events
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                # if the key is the listening socket, accept the connection
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                # else just service the connection
+                else:
+                    service_connection(key, mask)
+                    
+    except KeyboardInterrupt:
+        logging.error("KeyboardInterrupt, exiting at %s", time.strftime("%Y-%m-%d %H:%M:%S"))
+    finally:
+        sel.close()
+        sys.exit(0)
 
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
-                
-except KeyboardInterrupt:
-    logging.error("KeyboardInterrupt, exiting at %s", time.strftime("%Y-%m-%d %H:%M:%S"))
-finally:
-    sel.close()
+
+if __name__ == "__main__":
+    setup()
+    main_loop()
